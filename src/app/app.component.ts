7deba42284d53561143b81638107caf8969c0082
Carlos Mello Jr <cmellojr@gmail.com>
2024-05-06 12:21:13 -0300

    First commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+
+}
